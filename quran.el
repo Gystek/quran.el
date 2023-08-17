2d760f0514a06c400060475355045f29d24125e6
Gustek <gustek@riseup.net>
2023-08-17 18:25:35 +0200

    Add translations

--- quran.el
+++ quran.el
@@ -9,43 +9,42 @@
 
 ;; This file is not part of GNU Emacs.
 
-;;; Commentary: Depends on `curl' for downloading external files
+;;; Commentary: Depends on `curl' and `git' for downloading external files
 ;;; Code:
 
 ; Public
 
-(defvar quran-buffers nil
-  "Buffers opened by quran.el")
-
 (defvar quran-buffer-zoom 5
-  "The amount of zoom to pass to `text-scale-increase'")
+  "The amount of zoom to pass to `text-scale-increase'.")
 
 (defvar quran-font "Amiri Quran"
-  "The font for displaying the Arabic text of the Qur'an")
+  "The font for displaying the Arabic text of the Qur'an.")
 
 (defvar quran-type "uthmani"
   "The type of Qur'anic text to display.
-   Can be \"simple\" or \"uthmani\"")
+Can be \"simple\" or \"uthmani\"")
 
 (defvar quran-pause-marks t
-  "Whether or not to show the pause marks for recitation")
+  "Whether or not to show the pause marks for recitation.")
 
 (defvar quran-sajdah-signs t
-  "Whether or not to include sajdah (prostration) signs
-   in the Qur'anic text")
+  "Whether or not to include sajdah signs in the text.")
 
 (defvar quran-tatwil t
-  "Whether or not to include tatwil (elongation) below
-   superscript alifs")
+  "Whether or not to include tatwil below superscript alifs.")
 
 (defvar quran-rub nil
-  "Whether or not to include the rub-al-hizb (sign
-   indicating the quarter of a hizb, or the eighth
-   of a juz) in the Qur'anic text")
+  "Whether or not to include the rub-al-hizb in the text.
+The rub-al-hizb is a sign indicating the quarter of a
+hizb, or the eighth of a juz")
+
+(defvar quran-split-direction 'vertical
+  "Which way to split the frame when adding translations.
+Can be 'horizontal or 'vertical.")
 
 (defun quran-mode-enable ()
+  "Enable Qur'an mode for the current buffer."
   (interactive)
-  "Enable Qur'an mode for the current buffer"
   (if (not (member quran-type '("uthmani" "simple")))
       (error "Unknown `quran-type': %s" quran-type)
     (set-fontset-font "fontset-default" 'arabic quran-font)
@@ -56,8 +55,8 @@
     (visual-line-mode 1)))
 
 (defun quran-mode-disable ()
+  "Disable Qur'an mode in the current buffer."
   (interactive)
-  "Disable Qur'an mode in the current buffer"
   (setq buffer-read-only nil)
   (setq cursor-type t)
   (text-scale-increase 0)
@@ -66,16 +65,15 @@
 
 (defun quran-read (ref)
   "Read a surah or ayat of the Qur'an.
-
-  Reference syntax:
-  - Surah:
-    - <surah_number>
-    - <surah_name>
-  - Verse:
-    - <verse_num>
-    - <verse_num-verse_num>
-  - Full reference:
-    - <surah>[:<verse>[,<verse>]*]"
+Reference syntax:
+- Surah:
+  - <surah_number>
+  - <surah_name>
+- Verse:
+  - <verse_num>
+  - <verse_num-verse_num>
+- Full reference:
+  - <surah>[:<verse>[,<verse>]*]"
 
   (interactive "sReference: ")
   (pcase-let ((`(,surah . ,ayat) (__parse_reference ref)))
@@ -89,11 +87,16 @@
 (defun quran-close ()
   "Kill all buffers opened by quran.el and delete text files"
   (interactive)
-  (delete-file __arabic-text-path)
+  (unless (null __arabic-text-path)
+    (delete-file __arabic-text-path))
+  (unless (null __translation-texts-path)
+    (delete-directory __translation-texts-path :recursive t))
   (setq __arabic-text-path nil)
+  (setq __translation-texts-path nil)
   (dolist (buf quran-buffers)
-      (kill-buffer buf))
-  (setf quran-buffers nil))
+    (when (get-buffer (car buf))
+      (kill-buffer (car buf))))
+  (setq quran-buffers nil))
 
 ; Internals
 
@@ -106,6 +109,8 @@
     str))
 
 (defvar __arabic-text-path nil)
+(defvar __translation-texts-path nil)
+(defvar quran-buffers nil)
 
 (defun __make-download-link ()
   (format "https://tanzil.net/pub/download/index.php?%s%s%s%squranType=%s&outType=txt-2&agree=true"
@@ -146,16 +151,30 @@
       (setq surah-n (1+ (cl-position surah __surahs-latin :test (lambda (name elem) (string= name (car elem)))))))
     (if (> (length split0) 1)
 	(let ((ayat (string-split (nth 1 split0) ",")))
-	  `(,surah-n . ,(__expand-ayat ayat)))
-      `(,surah-n . nil))))
+	  (cons surah-n (__expand-ayat ayat)))
+      (cons surah-n nil))))
+
+(defun __quran-buffer (surah ayat)
+  (let ((elm (car (__get-elem (lambda (x) (string= (car x) surah))
+			 quran-buffers))))
+    (when (not (null elm))
+      (kill-buffer (car elm))
+      (setq quran-buffers (remove elm quran-buffers)))
+    (setq quran-buffers (cons (cons surah ayat) quran-buffers))
+    (switch-to-buffer surah)))
+
+(defun __get-elem (fun col &optional i)
+  (or i (setq i 0))
+  (if (null col)
+      nil
+    (if (funcall fun (car col))
+	(cons (car col) i)
+      (__get-elem fun (cdr col) (+ i 1)))))
 
 (defun __load-surah (num ayat)
   (cl-incf num -1)
-  (let ((bname (format "Surah %s" (car (nth num __surahs-latin)))))
-    (setq quran-buffers (cons bname quran-buffers))
-    (switch-to-buffer bname))
-  (quran-mode-disable)
-  (erase-buffer)
+  (__quran-buffer (format "Surah %s" (car (nth num __surahs-latin)))
+		  ayat)
   (insert-file-contents __arabic-text-path)
 
   ; Keep only this surah
@@ -174,14 +193,12 @@
   (unless (null ayat)
     (let ((length (cdr (nth num __surahs-latin)))
 	  (i 0))
-      (print ayat)
       (while (< i length)
 	(cl-incf i)
 	(if (member i ayat)
 	    (progn
 	      (insert (format "%s " (__number-to-arabic i)))
 	      (goto-line (+ (line-number-at-pos) 1)))
-	  (print i)
 	  (kill-whole-line)))))
 
   ; Remove ayah and surah numbering
@@ -201,6 +218,108 @@
     (move-beginning-of-line nil))
 
   (beginning-of-buffer))
+
+(defun __download-translations ()
+  (let* ((remote "https://github.com/Jomtek/quran-translated")
+	 (default-directory "/tmp")
+	 (path "/tmp/quran-translated"))
+    (setq __translation-texts-path path)
+    (when (not (file-exists-p path))
+	(save-window-excursion
+	  (make-process :name "Translations download"
+			:command (list "git"
+				       "clone"
+				       remote)
+			:stderr (get-buffer "*Messages*"))))))
+
+(eval-when-compile (require 'subr-x))
+
+(defun __select-translation ()
+  (let ((language (cdr (assoc (completing-read "Language: " (mapcar #'car __languages-codes)) __languages-codes))))
+    (when (null __translation-texts-path)
+      (let ((p (__download-translations)))
+	(while (eq (process-status p) 'run)
+	  (sleep-for 0.1))))
+    (let ((trans (completing-read "Translation: "
+				  (mapcar #'(lambda (x) (cons (capitalize (string-remove-prefix (concat language ".") x)) x))
+					  (seq-filter
+					   #'(lambda (x) (string-prefix-p language x))
+					   (directory-files __translation-texts-path))))))
+      (format "%s.%s" language (downcase trans)))))
+
+(defun quran-add-translation ()
+  "Adds a translation to the frame.
+You MUST be in the main arabic text buffer for this function
+to function properly."
+  (interactive)
+  (let ((trans (__select-translation)))
+    (unless (member trans (directory-files __translation-texts-path))
+      (error "Unknown translation: %s" trans))
+    (quran-add-translation-by-name trans)))
+
+(defun quran-remove-translation-by-name (translation)
+  "Remove a translation from the frame.
+This also deletes the translation buffer from memory.
+You MUST be in the main Arabic text buffer for this function
+to function properly."
+  (interactive "sTranslation name:")
+  (let ((bname (format "%s - %s"
+		       (buffer-name)
+		       translation)))
+    (delete-window (get-buffer-window bname))
+    (kill-buffer bname)
+    (setq quran-buffers (remove (car (__get-elem (lambda (x)
+						   (string= (car x) bname))
+						 quran-buffers))))))
+
+(defun quran-add-translation-by-name (translation)
+  "Add a translation to the frame by name.
+You MUST be in the main Arabic text buffer for this function
+to function properly."
+  (interactive "sTranslation name:")
+  (when (null __translation-texts-path)
+    (let ((p (__download-translations)))
+      (while (eq (process-status p) 'run)
+	(sleep-for 0.1))))
+  (if (eq quran-split-direction 'vertical)
+      (split-window-vertically)
+    (split-window-horizontally))
+
+  (other-window 1)
+  (let* ((surah (string-trim (buffer-name) "Surah "))
+	 (ayat (cdr (car (__get-elem (lambda (x)
+				       (string= (car x) (buffer-name)))
+				     quran-buffers))))
+	 (num (+ 1 (cdr (__get-elem (lambda (x)
+				      (string= (car x) surah))
+				    __surahs-latin)))))
+    (__quran-buffer (format "%s - %s"
+			    (buffer-name)
+			    translation)
+		    (cdr (car (__get-elem (lambda (x)
+					    (string= (car x) (buffer-name)))
+					  quran-buffers))))
+    (let ((tr-path (format "%s/%s/%d"
+			   __translation-texts-path
+			   translation
+			   num)))
+      (insert-file-contents tr-path))
+
+    (beginning-of-buffer)
+    (unless (null ayat)
+      (let ((length (cdr (nth num __surahs-latin)))
+	    (i 0))
+	(while (< i length)
+	  (cl-incf i)
+	  (if (member i ayat)
+	      (progn
+		(insert (format "%d. " i))
+		(goto-line (+ (line-number-at-pos) 1)))
+	    (kill-whole-line)))))
+    (beginning-of-buffer)
+    (setq buffer-read-only t)
+    (toggle-truncate-lines -1)
+    (visual-line-mode 1)))
 
 (defvar __surahs-latin
   '(("al-Fātiḥah" . 7)
@@ -317,5 +436,51 @@
     ("al-ʾIkhlāṣ" . 4)
     ("al-Falaq" . 5)
     ("an-Nās" . 6)))
+
+(defvar __languages-codes '(("አማርኛ" "am")
+						  ("العربية" "ar")
+						  ("Azərbaycan dili" "az")
+						  ("ⵜⴰⵎⴰⵣⵉⵖⵜ" "ber")
+						  ("Български" "bg")
+						  ("বাংলা" "bn")
+						  ("Bosanski" "bs")
+						  ("Čestina" "cs")
+						  ("Deutsch " "de")
+						  ("ދިވެހިބަސް" "dv")
+						  ("English" "en")
+						  ("Español" "es")
+						  ("فارسى" "fa")
+						  ("Français" "fr")
+						  ("هَوُسَ" "ha")
+						  ("हिन्दी" "hi")
+						  ("Bahasa Indonesia" "id")
+						  ("Italiano" "it")
+						  ("日本語" "ja")
+						  ("조선말" "ko")
+						  ("كوردی" "ku")
+						  ("മലയാളം" "ml")
+						  ("Bahasa Melayu" "ms")
+						  ("Nederlands" "nl")
+						  ("Norsk" "no")
+						  ("Phonetic" "phonetic")
+						  ("Polski" "pl")
+						  ("پښتو" "ps")
+						  ("Português" "pt")
+						  ("Română" "ro")
+						  ("Русский" "ru")
+						  ("سنڌي" "sd")
+						  ("Soomaaliga" "so")
+						  ("Shqip" "sq")
+						  ("Svenska" "sv")
+						  ("Kiswahili" "sw")
+						  ("தமிழ்" "ta")
+						  ("тоҷикӣ" "tg")
+						  ("ไทย" "th")
+						  ("Türkçe" "tr")
+						  ("татарча" "tt")
+						  ("ئۇيغۇرچ" "ug")
+						  ("اردو" "ur")
+						  ("Ўзбек" "uz")
+						  ("中文" "zh")))
 
 ;;; quran.el ends here
